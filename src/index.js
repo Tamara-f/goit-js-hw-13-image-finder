c1622a44411eebfe1f9ef0beecf1340c56ad2f90
tamara-f <filatovatam@ukr.net>
2020-07-25 12:19:22 +0300

    added extra task for homework-13

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import './styles.css';
 import imgsTpl from './templates/imgs.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import { alert } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
 
+// document.cookie = 'SameSite=None; Secure';
+// console.log(document.cookie);
 const refs = {
   form: document.querySelector('.js-search-form'),
   gallery: document.querySelector('#gallery'),
   loadmore: document.querySelector('#loadmore'),
 };
-refs.loadmore.classList.add('notactive');
+
 let inputValue;
 let pageNumber = 1;
 
+//for submit button
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
+  refs.loadmore.classList.add('notactive');
   refs.gallery.innerHTML = '';
   if (event.currentTarget.elements.query.value === '') {
     refs.gallery.innerHTML = '';
@@ -22,6 +30,7 @@ refs.form.addEventListener('submit', event => {
   }
 });
 
+//for fetch
 function startFetch() {
   const API_KEY = '17615021-9599c8e133abdd89b923fc662';
   const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${encodeURIComponent(
@@ -31,18 +40,42 @@ function startFetch() {
   fetch(URL)
     .then(res => res.json())
     .then(data => {
-      createGallery(data);
-      //   console.log(data);
-
-      refs.loadmore.classList.remove('notactive');
+      console.log(data);
+      if (data.total === 0) {
+        alert('Found 0 matches! Please enter correct search term');
+      } else createGallery(data);
+      if (data.hits.length < 12 && data.hits.length > 0) {
+        refs.loadmore.classList.add('notactive');
+        alert(
+          `All pictures are already shown. Found ${data.totalHits} matches!`,
+        );
+      }
     });
 }
 
+//for gallery insert
 function createGallery(data) {
   const markup = imgsTpl(data.hits);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+  refs.loadmore.classList.remove('notactive');
 }
 
+// for modal
+refs.gallery.addEventListener('click', event => {
+  console.log(event.target.dataset.source);
+  const instance = basicLightbox.create(`  <div class="modal">
+  <a><i class="material-icons">highlight_off</i></a>
+  <img src="${event.target.dataset.source}" >  
+</div> 
+`);
+  {
+    instance.element().querySelector('a').onclick = instance.close;
+  }
+
+  instance.show();
+});
+
+//for button load more
 refs.loadmore.addEventListener('click', event => {
   pageNumber += 1;
 
